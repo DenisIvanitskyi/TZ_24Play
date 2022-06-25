396c2f09c5115f0f87ffc6a107a092974f8ca731
Denis Ivaniyskyi <dev.denis.ivanitskyi@gmail.com>
2022-06-25 11:41:28 +0300

    Begin setup game bootstrapper

---
+++ Assets/Game/GameBootstrapper.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.Game
+{
+    public class GameBootstrapper : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _heroPrefab, _trackGround, _cubeWall, _cubePoint, _collectCubeText;
+
+        public void Start()
+        {
+            
+        }
+    }
+}
